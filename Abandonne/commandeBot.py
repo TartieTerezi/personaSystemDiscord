6b8a3e545e0598bcd7af0972bd3d1717ce2bf7db
Crepiliere <acrepilierepokemon@gmail.com>
2023-09-13 06:14:04 +0200

    remis a plus tard
    - Remet a plus tard certaines commandes pour plus tard.

---
+++ Abandonne/commandeBot.py
@@ -0,0 +1,122 @@
+﻿
+
+###### DATE ######
+
+@bot.hybrid_command(name="date",with_app_command=True,description="Donne la date du jour")
+async def _date(ctx):
+	await ctx.send(Embed.showDate(date))
+
+@bot.hybrid_command(name="skipday",with_app_command=True,description="passe au jours suivant")
+async def _skipday(ctx,daytoskip = 1):
+	date.skipDay(daytoskip)
+	await ctx.send(Embed.showDate(date))
+
+@bot.hybrid_command(name="skipstep",with_app_command=True,description="passe au jours suivant")
+async def _setstep(ctx,steptoskip = 1):
+	date.skipStep(steptoskip)
+	await ctx.send(Embed.showDate(date))
+
+
+#persona 
+
+@bot.hybrid_command(name="personalist",with_app_command=True, description="Liste des personas")
+async def _personalist(ctx,page : int = 1):
+	listPersonaPage,listEmojisPage,pageCurrent,maxPage = utils.listToShow(ctx,listPersonas,page)
+
+	embed=discord.Embed(title="Liste des personas "+ str(pageCurrent) +"/"+ str(maxPage))
+
+	for onePersona in listPersonaPage:
+		embed.add_field(name="",value=onePersona.nom, inline=True)
+	mess = await ctx.send(embed=embed)
+
+	await utils.setMessageEmotes(mess,listEmojisPage)
+
+	try:
+		isValidEmote,indexValidEmote = await utils.getReaction(bot,mess,listPersonaPage)
+
+		if(isValidEmote):
+			#embded avec les informations de la persona 
+			persona = listPersonaPage[indexValidEmote]
+			await ctx.send(embed=Embed.showPersona(persona))
+
+	except asyncio.TimeoutError:
+		await mess.add_reaction('🕐')
+	else:
+		await mess.delete()
+
+
+@bot.hybrid_command(name="newskill", with_app_command=True, description="crée une nouvelle competence.")
+async def _newSkill(ctx,nom : str,element : int,description : str,cout : int,puissance : int,precision : int):
+	newSkillToAdd = Skill(nom,element,description,cout,puissance,precision)
+	listSkill.append(newSkillToAdd)
+	file.newSkill(newSkillToAdd)
+
+@bot.hybrid_command(name="addskill", with_app_command=True, description="Ajoute un nouvelle competence a ta persona.")
+async def _addskill(ctx,nom):
+	isFind = False
+
+	for oneSkill in listSkill:
+		if(oneSkill.nom == nom):
+			isFind = True
+
+			isAlreadyLearned = False
+			for skillAlreadyLearn in persona.skills:
+				if(skillAlreadyLearn.nom == oneSkill.nom):
+					isAlreadyLearned = True
+
+			if(isAlreadyLearned == False):
+				persona.skills.append(oneSkill)
+				await ctx.send(embed=Embed.showNewSkill(persona,oneSkill))
+			else:
+				await ctx.send("Attaque "+ oneSkill.nom + " déjà apprise")
+
+			break
+
+	if(isFind == False):
+		await ctx.send("Aucune attaque trouvé sous le nom de " + str(nom))
+
+
+###### DIALOGUE #####
+
+@bot.hybrid_command(name="dialogue",with_app_command=True, description="Dialogue")
+async def _dialogue(ctx,arg):
+	embed=discord.Embed(color=0xe81162)
+
+	if(len(arg)>1024):
+		await ctx.send("impossible d'envoyer un message aussi long")
+	else:
+		embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/1118247026635853958/1119009981119856640/tartie_base.png")
+		#embed.set_image(url="https://cdn.discordapp.com/attachments/1093990697717215292/1113807915128717413/image.png")
+		for oneCharacter in listCharacters:
+			if(ctx.author.id == oneCharacter.id):
+				embed.add_field(name=oneCharacter.nom +" "+ oneCharacter.prenom, value=str(arg), inline=False)
+		await ctx.send(embed=embed)
+
+
+#(item)
+
+@bot.hybrid_command(name="itemlist",with_app_command=True, description="Liste des objets")
+async def _itemlist(ctx,page : int = 1):
+	listItemsPage,listEmojisPage,pageCurrent,maxPage = utils.listToShow(ctx,listItem,page)
+
+	embed=discord.Embed(title="Liste des Items "+ str(pageCurrent) +"/"+ str(maxPage))
+
+	for oneItem in listItemsPage:
+		embed.add_field(name="",value=oneItem.nom, inline=True)
+	mess = await ctx.send(embed=embed)
+
+	await utils.setMessageEmotes(mess,listEmojisPage)
+
+	try:
+		isValidEmote,indexValidEmote = await utils.getReaction(bot,mess,listItemsPage)
+
+		if(isValidEmote):
+			#embded avec les informations de la persona 
+			item = listItemsPage[indexValidEmote]
+			embed=discord.Embed(title=item.nom, description=item.info)
+			await ctx.send(embed=embed)
+
+	except asyncio.TimeoutError:
+		await mess.add_reaction('🕐')
+	else:
+		await mess.delete()
--- frogyCat.py
+++ frogyCat.py
@@ -81,22 +81,6 @@ async def _reset(ctx):
 	listSkill,listPersonas,listCharacters,date,listItem = file.reset()
 	await ctx.send("Update de tout les elements")
 
-###### DATE ######
-
-@bot.hybrid_command(name="date",with_app_command=True,description="Donne la date du jour")
-async def _date(ctx):
-	await ctx.send(Embed.showDate(date))
-
-@bot.hybrid_command(name="skipday",with_app_command=True,description="passe au jours suivant")
-async def _skipday(ctx,daytoskip = 1):
-	date.skipDay(daytoskip)
-	await ctx.send(Embed.showDate(date))
-
-@bot.hybrid_command(name="skipstep",with_app_command=True,description="passe au jours suivant")
-async def _setstep(ctx,steptoskip = 1):
-	date.skipStep(steptoskip)
-	await ctx.send(Embed.showDate(date))
-
 ###### CHARACTER ######
 
 @bot.hybrid_command(name="stat", with_app_command=True, description="Montre vos statistique")
@@ -161,30 +145,7 @@ async def _statpersona(ctx,user: discord.User = None):
 	else:
 		await ctx.send("Aucune persona trouvé.")
 
-@bot.hybrid_command(name="personalist",with_app_command=True, description="Liste des personas")
-async def _personalist(ctx,page : int = 1):
-	listPersonaPage,listEmojisPage,pageCurrent,maxPage = utils.listToShow(ctx,listPersonas,page)
 
-	embed=discord.Embed(title="Liste des personas "+ str(pageCurrent) +"/"+ str(maxPage))
-
-	for onePersona in listPersonaPage:
-		embed.add_field(name="",value=onePersona.nom, inline=True)
-	mess = await ctx.send(embed=embed)
-
-	await utils.setMessageEmotes(mess,listEmojisPage)
-
-	try:
-		isValidEmote,indexValidEmote = await utils.getReaction(bot,mess,listPersonaPage)
-
-		if(isValidEmote):
-			#embded avec les informations de la persona 
-			persona = listPersonaPage[indexValidEmote]
-			await ctx.send(embed=Embed.showPersona(persona))
-
-	except asyncio.TimeoutError:
-		await mess.add_reaction('🕐')
-	else:
-		await mess.delete()
 
 ###### SKILL ######
 
@@ -197,35 +158,6 @@ async def _skill(ctx, skill_name):
 	else:
 		await ctx.send("Aucune attaque trouvé sous le nom de " + str(skill_name))
 
-@bot.hybrid_command(name="newskill", with_app_command=True, description="crée une nouvelle competence.")
-async def _newSkill(ctx,nom : str,element : int,description : str,cout : int,puissance : int,precision : int,is_healing : bool):
-	newSkillToAdd = Skill(nom,element,description,cout,puissance,precision,is_healing)
-	listSkill.append(newSkillToAdd)
-	file.newSkill(newSkillToAdd)
-
-@bot.hybrid_command(name="addskill", with_app_command=True, description="Ajoute un nouvelle competence a ton persona.")
-async def _addskill(ctx,nom):
-	isFind = False
-
-	for oneSkill in listSkill:
-		if(oneSkill.nom == nom):
-			isFind = True
-
-			isAlreadyLearned = False
-			for skillAlreadyLearn in persona.skills:
-				if(skillAlreadyLearn.nom == oneSkill.nom):
-					isAlreadyLearned = True
-
-			if(isAlreadyLearned == False):
-				persona.skills.append(oneSkill)
-				await ctx.send(embed=Embed.showNewSkill(persona,oneSkill))
-			else:
-				await ctx.send("Attaque "+ oneSkill.nom + " déjà apprise")
-
-			break
-
-	if(isFind == False):
-		await ctx.send("Aucune attaque trouvé sous le nom de " + str(nom))
 
 @bot.hybrid_command(name="skilllist",with_app_command=True, description="Liste des competences")
 async def _skillList(ctx,page : int = 1):	
@@ -290,32 +222,6 @@ async def _listchannel(ctx):
 
 ###### ITEM ######
 
-@bot.hybrid_command(name="itemlist",with_app_command=True, description="Liste des objets")
-async def _itemlist(ctx,page : int = 1):
-	listItemsPage,listEmojisPage,pageCurrent,maxPage = utils.listToShow(ctx,listItem,page)
-
-	embed=discord.Embed(title="Liste des Items "+ str(pageCurrent) +"/"+ str(maxPage))
-
-	for oneItem in listItemsPage:
-		embed.add_field(name="",value=oneItem.nom, inline=True)
-	mess = await ctx.send(embed=embed)
-
-	await utils.setMessageEmotes(mess,listEmojisPage)
-
-	try:
-		isValidEmote,indexValidEmote = await utils.getReaction(bot,mess,listItemsPage)
-
-		if(isValidEmote):
-			#embded avec les informations de la persona 
-			item = listItemsPage[indexValidEmote]
-			embed=discord.Embed(title=item.nom, description=item.info)
-			await ctx.send(embed=embed)
-
-	except asyncio.TimeoutError:
-		await mess.add_reaction('🕐')
-	else:
-		await mess.delete()
-
 @bot.hybrid_command(name="take",with_app_command=True,description="Prendre un objet s'il est proche")
 async def _take(ctx,objettotake):
 	#for unLieu in listLieu:
@@ -343,21 +249,7 @@ async def _inventaire(ctx):
 			for oneItem in oneCharacter.inventaire:
 				await ctx.send(str(oneItem.nom) + " x" + str(oneCharacter.inventaire[oneItem]))
 
-###### DIALOGUE #####
 
-@bot.hybrid_command(name="dialogue",with_app_command=True, description="Dialogue")
-async def _dialogue(ctx,arg):
-	embed=discord.Embed(color=0xe81162)
-
-	if(len(arg)>1024):
-		await ctx.send("impossible d'envoyer un message aussi long")
-	else:
-		embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/1118247026635853958/1119009981119856640/tartie_base.png")
-		#embed.set_image(url="https://cdn.discordapp.com/attachments/1093990697717215292/1113807915128717413/image.png")
-		for oneCharacter in listCharacters:
-			if(ctx.author.id == oneCharacter.id):
-				embed.add_field(name=oneCharacter.nom +" "+ oneCharacter.prenom, value=str(arg), inline=False)
-		await ctx.send(embed=embed)
 
 ###### GROUPE ######
 
@@ -420,8 +312,6 @@ async def _statgroupe(ctx):
 	await ctx.send(embed=Embed.showGroupe(groupe))
 
 ###### FIGHT ######
-
-
 
 @bot.hybrid_command(name="startfightmob",with_app_command=True, description="Initie un combat contre un mob")
 async def _startfightmob(ctx):
