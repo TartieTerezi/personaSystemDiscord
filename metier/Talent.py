890d46b3f09ea9be74f35dc6ef1af5c9fcd2b110
Crepiliere <58311742+TartieTerezi@users.noreply.github.com>
2023-10-24 16:59:45 +0200

    Create Talent.py
    Liste des differents trigger d'activation du Talent.

---
+++ metier/Talent.py
@@ -0,0 +1,16 @@
+
+# classe des skill talent
+
+# Trigger des activations des talents
+# Quand tu attaques
+# Quand tu te buff
+# quand tu te debuff
+# en debut de combat
+# en fin de combat 
+# quand tu te protege
+# Quand un alié revit 
+# quand un allié meurt
+# quand un ennemis meurt
+# Quand un ennemis apparait en combat
+# a la fin de chaque tour
+# 
