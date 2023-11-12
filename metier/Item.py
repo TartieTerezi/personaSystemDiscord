16c7dfa7b2e5ba113a883b50f943c7e7dfccfb0e
Crepiliere <acrepilierepokemon@gmail.com>
2023-11-12 04:35:23 +0100

    Update Item.py

--- metier/Item.py
+++ metier/Item.py
@@ -1,28 +1,52 @@
 class Item(object):
-	"""docstring for Item"""
+	"""
+		Un Item la base de tout les Items du rp. 
+	"""
 	def __init__(self,index : int,nom : str,info : str = ""):
-		self.id = index
-		self.nom = nom
-		self.info = info
+		self.id : int = index  
+		""" Index de l'item dans la bdd. """
+		self.nom : str = nom 
+		""" Nom de l'item. """
+		self.info : str = info 
+		""" Information sur l'Item. """
 
-	def is_useable(self):
+	def is_useable(self) -> bool:
+		"""
+			Retourne un boolean Vrai ou Faux selon si l'objet peut etre utilise.
+		"""
 		return False
 
-	def is_equipeable(self):
+	def is_equipeable(self) -> bool:
+		"""
+			Retourne un boolean Vrai ou Faux selon si l'objet peut etre equipe.
+		"""
 		return False
 
-	def equip(self,character):
+	def equip(self,character) -> None:
+		"""
+			Equipe l'objet a un personnage de type Character.
+		"""
 		pass
 
-	def __str__(self):
+	def use(self,character) -> None:
+		"""
+			utilise l'objet sur un personnage de type Character.
+		"""
+		pass
+
+	def __str__(self) -> str:
 		return f"(id={self.id},nom={self.nom},info={self.info})"
 
 class Weapon(Item):
-	"""docstring for Weapon"""
+	"""
+		Arme pouvant etre equipe par des personnage, inclue une precision et une puissance d'attaque
+	"""
 	def __init__(self,index : int,nom : str,puissance : int, precision : int,info : str = ""):
 		Item.__init__(self,index,nom,info)
-		self.puissance = puissance
+		self.puissance : int = puissance
+		""" Puissance de l'arme. """
 		self.precision = precision
+		"""" Precision de l'arme. """
 
 	def is_equipeable(self):
 		return True
@@ -35,12 +59,17 @@ class Weapon(Item):
 		return f"(id={self.id},nom={self.nom},power={self.puissance},precision={self.precision},info={self.info})"
 
 class HealingObject(Item):
-	"""docstring for healing object"""
+	"""
+		Objet pouvant soigner les pv et / ou les pc
+	"""
 	def __init__(self,index : int,nom : str,pvHeal : int,pcHeal : int,isPercent : int = False,info : str = ""):
 		Item.__init__(self,index,nom,info)
-		self.pvHeal = pvHeal
+		self.pvHeal : int = pvHeal
+		""" Puissance de soin des pv. """
 		self.pcHeal = pcHeal
-		self.isPercent = isPercent
+		""" Puissance de soin des pc. """
+		self.isPercent : bool = isPercent
+		""" Determine si le soin est affecte en pourcentage ou de façon 'statique'. """
 
 	def use(self,character):
 		if(self.isPercent):
